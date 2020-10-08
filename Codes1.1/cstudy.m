9c64470bf1997f633a2141f04609d39c35307bc4
Andreas Kloeckner <inform@tiker.net>
2020-10-08 16:44:22 -0500

    Cstudy: rate estimation

--- Codes1.1/cstudy.m
+++ Codes1.1/cstudy.m
@@ -1,6 +1,11 @@
 for order = 1:8
+        errs = zeros(3,4);
         for ref = 0:2
                 disp(sprintf("order %d refinement %d:", order, ref))
-                disp(vortex(order, ref)');
+                err = vortex(order, ref);
+                disp(err');
+                errs(ref+1, :) = err';
         end
+        raterho = log(errs(end, 1)/errs(1, 1))/log(4)
+        raterhou = log(errs(end, 2)/errs(1, 2))/log(4)
 end
